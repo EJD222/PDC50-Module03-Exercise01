ff38305088f675bdb57fc66b7f5dc00fb4aee023
EJD222 <eljdiaz500@gmail.com>
2024-09-04 13:57:45 +0800

    Improve behavior of Retry and Exit buttons

--- Module03Exercise01/View/OfflinePage.xaml.cs
+++ Module03Exercise01/View/OfflinePage.xaml.cs
@@ -6,8 +6,28 @@ public partial class OfflinePage : ContentPage
 	{
 		InitializeComponent();
 	}
-    private void OnExitButtonClicked(object sender, EventArgs e)
+
+    private async void OnRetryButtonClicked(object sender, EventArgs e)
     {
-        System.Environment.Exit(0);
+        var current = Connectivity.NetworkAccess;
+        if (current == NetworkAccess.Internet)
+        {
+            await DisplayAlert("Connected", "Your internet connection was restored.", "Ok");
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+
+        else
+        {
+            await DisplayAlert("No Internet", "Please check your internet connection and try again.", "Ok");
+        }
+    }
+    private async void OnExitButtonClicked(object sender, EventArgs e)
+    {
+        bool isConfirmed = await DisplayAlert("Confirm Exit", "Are you sure you want to exit the application?", "Yes", "No");
+
+        if (isConfirmed)
+        {
+            System.Environment.Exit(0);
+        }
     }
 }
